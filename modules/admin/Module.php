df2465c4a4651b4255d9f23ce99f129c9c541637
Keeller <keellere@rambler.ru>
2019-06-15 01:18:52 +0400

    Admin crud finished

---
+++ modules/admin/Module.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\modules\admin;
+
+/**
+ * admin module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\admin\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+        \Yii::$app->setLayoutPath('@app/modules/admin/views/layouts');
+        \Yii::$app->layout = 'main';
+
+        // custom initialization code goes here
+    }
+}
